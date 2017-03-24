3dbf0e1b58d92c9d02ad15ed55cc3e3859a5951d
Bruno Fagundes <bruno.fvpp@gmail.com>
2017-03-24 02:25:15 -0300

    add gist in profile detail

--- js/services/githubAPIService.js
+++ js/services/githubAPIService.js
@@ -1,21 +1,31 @@
 angular.module("miniSearch").factory('githubAPI', function($http) {
 
   var _getUsers = function(searchTerm) {
-    return $http.get('https://api.github.com/search/users', { params: { q: searchTerm, per_page: "100" } });
+    return $http.get('https://api.github.com/search/users', {
+      params: {
+        q: searchTerm,
+        per_page: "100"
+      }
+    });
   };
 
   var _getUserDetail = function(user) {
-    return $http.get('https://api.github.com/users/'+user);
+    return $http.get('https://api.github.com/users/' + user);
   };
 
   var _getUserRepos = function(user) {
-    return $http.get('https://api.github.com/users/'+user+'/repos');
+    return $http.get('https://api.github.com/users/' + user + '/repos');
+  };
+
+  var _getUserGists = function(user) {
+    return $http.get('https://api.github.com/users/' + user + '/gists');
   };
 
   return {
     getUsers: _getUsers,
     getUserDetail: _getUserDetail,
-    getUserRepos: _getUserRepos
+    getUserRepos: _getUserRepos,
+    getUserGists: _getUserGists
   };
 
 });
