6ad8d3c9cd5c01f75ee9becd9879406f01945627
Bruno Fagundes <bruno.fvpp@gmail.com>
2017-03-24 02:25:15 -0300

    add gist in profile detail

--- app/js/controllers/userDetailCtrl.js
+++ app/js/controllers/userDetailCtrl.js
@@ -1,19 +1,30 @@
-angular.module("miniSearch").controller("userDetailCtrl", function ($scope, $routeParams, githubAPI) {
+angular.module("miniSearch").controller("userDetailCtrl", function($scope, $routeParams, githubAPI) {
   var user = $routeParams.user;
 
-  githubAPI.getUserDetail(user).then(function (response) {
-      $scope.user = response.data;
+  githubAPI.getUserDetail(user).then(function(response) {
+    $scope.user = response.data;
   }, function(data) {
-      $scope.message = "Aconteceu um problema: " + data;
+    $scope.message = "Aconteceu um problema: " + data;
   });
 
-  githubAPI.getUserRepos(user).then(function (response) {
-      $scope.repos = response.data;
-      angular.forEach($scope.repos, function(value, key) {
-        value.description = value.description || "Sem drescrição";
-      });
+  githubAPI.getUserRepos(user).then(function(response) {
+    $scope.repos = response.data;
+    angular.forEach($scope.repos, function(value, key) {
+      value.description = value.description || "Sem drescrição";
+    });
   }, function(data) {
-      $scope.message = "Aconteceu um problema: " + data;
+    $scope.message = "Aconteceu um problema: " + data;
+  });
+
+  githubAPI.getUserGists(user).then(function(response) {
+    $scope.gists = response.data;
+    angular.forEach($scope.gists, function(value, key) {
+      for(name in value.files){
+        value.name = name;
+      }
+    });
+  }, function(data) {
+    $scope.message = "Aconteceu um problema: " + data;
   });
 
 });
